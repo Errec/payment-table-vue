b3c64ad57371710cfb2f4dacea4399bf0ae6904f
Raniro Coelho <errecme@gmail.com>
2018-12-16 19:16:20 +0000

    Add table view

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <router-link to="table" tag="button">Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
